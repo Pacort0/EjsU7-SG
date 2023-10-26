e19794ab745e90f3ef0acb276284f7e5f99c0516
siu <francisco.rodriguez@iesnervion.es>
2023-10-26 11:09:52 +0200

    Agregar archivos de proyecto.

---
+++ EjsU7-SG/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using EjsU7_SG.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EjsU7_SG.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var fechaActual = DateTime.Now;
+            var message = "Buenos días";
+
+            if (fechaActual.Hour >= 14)
+            {
+                message = "Buenas tardes";
+            } else if (fechaActual.Hour >= 21)
+            {
+                message = "Buenas noches";
+            }
+
+            ViewData["Message"] = message;
+            ViewBag.fechaLarga = fechaActual.ToLongDateString();
+            return View(persona);
+        }
+
+        private clsPersona persona = new clsPersona()
+        {
+            Id = 1,
+            Nombre = "Francisco",
+            Apellidos = "Rodríguez",
+            Direccion = "Mi calle",
+            FechaNac = new DateTime(2003, 04, 27),
+            Tlfn = 654444089,
+        };
+    }
+
+}
---
+++ EjsU7-SG/Models/clsPersona.cs
@@ -0,0 +1,81 @@
+﻿namespace EjsU7_SG.Models
+{
+    public class clsPersona
+    {
+        #region atributos
+        private String nombre;
+        private String apellidos;
+        private DateTime fechaNac;
+        private long tlfn;
+        private int id;
+        #endregion
+
+        #region constructores
+        public clsPersona()
+        {
+            nombre = "";
+            apellidos = "";
+        }
+
+        public clsPersona(int id, string nombre, string apellidos)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+        }
+
+        public clsPersona(int id, string nombre, string apellidos, DateTime fechaNac, long tlfn)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.tlfn = tlfn;
+            this.fechaNac = fechaNac;
+        }
+        #endregion
+
+        #region propiedades
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public String Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public DateTime FechaNac
+        {
+            get { return fechaNac; }
+            set { fechaNac = value; }
+        }
+        public long Tlfn
+        {
+            get { return tlfn; }
+            set { tlfn = value; }
+        }
+        public String NombreCompleto
+        {
+            get { return $"{nombre} {apellidos}"; }
+        }
+        public String Direccion { get; set; }
+        #endregion
+        #region funciones y metodos
+        ///<summary>
+        ///Funcion que devuelve la concatenacion del atributo nombre y el atributo apellido
+        ///Precondiciones: ninguna
+        ///Postcondiciones: primera letra de cada palabra sea mayúscula, que no sea null... etc.
+        ///</summary>
+        ///<returns>string con el nombre completo</returns>
+        public String FuncionNombreCompleto()
+        {
+            return $"Su nombre completo es: {nombre} {apellidos}";
+        }
+        #endregion
+    }
+}
