f221108f554c1b9ae42b74fb9b9cdcb87e99ab65
siu <francisco.rodriguez@iesnervion.es>
2023-11-15 13:23:32 +0100

    K.ZDFJR

--- EjsU7-SG/Models/ViewModel/clsPersonaDepartamento.cs
+++ EjsU7-SG/Models/ViewModel/clsPersonaDepartamento.cs
@@ -5,32 +5,20 @@ namespace EjsU7_SG.Models.ViewModel
 {
     public class clsPersonaDepartamento:clsPersona
     {
-        #region atributos
-        private List<clsPersona> listaPersonas;
-        private List<clsDepartamento> listaDepartamentos;
-        #endregion
-
-        #region constructores
-        public clsPersonaDepartamento()
-        {
-            listaPersonas = clsListadoPersonas.listadoPersonas();
-            listaDepartamentos = clsListadoDepartamentos.listadoDepartamentos();
-        }
-        #endregion
-
         #region propiedades
         public List<clsDepartamento> ListaDepartamentos
         {
-            get { return listaDepartamentos; }
-
-        }
-        public List<clsPersona> ListaDeptPersonas
-        {
-            get
-            {
-                return listaPersonas;
-            }
+            get;
         }
         #endregion
+        #region constructores
+        public clsPersonaDepartamento()
+        {
+            ListaDepartamentos = clsListadoDepartamentos.listadoDepartamentos();
+            Nombre = "Ivan";
+            Apellidos = "Rakitic";
+        }
+        #endregion
+
     }
 }
